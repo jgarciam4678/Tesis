b1096ccc02c9c32921ad2672c07407b010be97f6
jgarciam4678 <jesus.garciam@pucp.edu.pe>
2017-04-24 18:35:27 -0500

    Update hostapd_cli.c

--- hostapd/hostapd_cli.c
+++ hostapd/hostapd_cli.c
@@ -21,7 +21,7 @@
 
 static const char *const hostapd_cli_version =
 "hostapd_cli v" VERSION_STR "\n"
-"Copyright (c) 2004-2017, Jouni Malinen <j@w1.fi> and contributors";
+"Copyright (c) 2004-2017, Jouni Malinen <j@w1.fi> and Jesus' contributions";
 
 static struct wpa_ctrl *ctrl_conn;
 static int hostapd_cli_quit = 0;
