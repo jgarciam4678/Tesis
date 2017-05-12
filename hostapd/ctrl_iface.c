5baef977181a14266d78b52d32298064c4819860
jgarciam4678 <jesus.garciam@pucp.edu.pe>
2017-05-12 15:31:38 -0500

    Update ctrl_iface.c

--- hostapd/ctrl_iface.c
+++ hostapd/ctrl_iface.c
@@ -2598,7 +2598,7 @@ static int hostapd_ctrl_iface_receive_process(struct hostapd_data *hapd,
 			hapd, reply, reply_size);
 #endif /* NEED_AP_MLME */
 	} else if (os_strcmp(buf, "PMKSA") == 0) {
-		reply_len = hostapd_ctrl_iface_pmksa_list_mesh(hapd, addr,
+		reply_len = hostapd_ctrl_iface_pmksa_list_mesh(hapd, reply,
 							  reply_size);
 	} else if (os_strcmp(buf, "PMKSA_FLUSH") == 0) {
 		hostapd_ctrl_iface_pmksa_flush(hapd);
