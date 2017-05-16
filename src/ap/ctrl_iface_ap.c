e8ad836b2f8c30c90fa3019a35c1dfc393ad2c6c
jgarciam4678 <jesus.garciam@pucp.edu.pe>
2017-05-16 18:30:02 -0500

    Update ctrl_iface_ap.c

--- src/ap/ctrl_iface_ap.c
+++ src/ap/ctrl_iface_ap.c
@@ -700,9 +700,9 @@ void * hostapd_ctrl_iface_pmksa_create_entry(const u8 *aa, char *cmd)
 	return wpa_auth_pmksa_create_entry(aa, spa, pmk, pmkid, expiration);
 }
 
-void hostapd_ctrl_iface_ap_pmksa_add(struct hostapd_data *hapd, char *buf)
+int hostapd_ctrl_iface_ap_pmksa_add(struct hostapd_data *hapd, char *buf)
 {
-	wpas_ctrl_iface_pmksa_add(hapd->wpa_auth, buf);
+	return wpas_ctrl_iface_pmksa_add(hapd->wpa_auth, buf);
 }
 
 #ifdef CONFIG_PMKSA_CACHE_EXTERNAL
