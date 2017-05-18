685ac15652f0d94c46ffa5ef12d8b5646c8bd8ef
jgarciam4678 <jesus.garciam@pucp.edu.pe>
2017-05-18 17:26:34 -0500

    Update ctrl_iface_ap.h

--- src/ap/ctrl_iface_ap.h
+++ src/ap/ctrl_iface_ap.h
@@ -36,7 +36,7 @@ int hostapd_ctrl_iface_pmksa_list_mesh(struct hostapd_data *hapd,
 				       const u8 *addr, char *buf, size_t len);
 void * hostapd_ctrl_iface_pmksa_create_entry(const u8 *aa, char *cmd);
 
-static int hostapd_ap_pmksa_cache_add_external(struct hostapd_data *hapd,
+int hostapd_ap_pmksa_cache_add_external(struct hostapd_data *hapd,
 				     char *buf);
 
 #endif /* CTRL_IFACE_AP_H */
