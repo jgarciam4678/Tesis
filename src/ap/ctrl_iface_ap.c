c2758b377e4c895e309fb42135fe9546e3a1c74a
jgarciam4678 <jesus.garciam@pucp.edu.pe>
2017-05-18 12:04:07 -0500

    Update ctrl_iface_ap.c

--- src/ap/ctrl_iface_ap.c
+++ src/ap/ctrl_iface_ap.c
@@ -783,7 +783,7 @@ static int wpas_ap_pmksa_cache_add_external(hostapd_data *hapd,
 	*/
 	entry->expiration = now.sec + 20000;
 	
-	wpa_sm_pmksa_cache_add_entry(wpa_s->wpa, entry);
+	wpa_sm_pmksa_cache_add_entry(hapd, entry);
 	entry = NULL;
 	ret = 0;
 fail:
