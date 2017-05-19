98c73240b2e2be10515b7cc09a1ed5394861eeae
jgarciam4678 <jesus.garciam@pucp.edu.pe>
2017-05-18 20:00:28 -0500

    Update hostapd_cli.c

--- hostapd/hostapd_cli.c
+++ hostapd/hostapd_cli.c
@@ -1303,7 +1303,11 @@ static int helloworld(struct wpa_ctrl *ctrl, int argc, char *argv[])
 
 static int hostapd_new_entry(struct wpa_ctrl *ctrl, int argc, char *argv[])
 {
-	return wpa_ctrl_command(ctrl, "MOBILE");
+	if (argc != 1) {
+		printf("Invalid MOBILE command: needs 1 arguments\n");
+		return -1;
+	}
+	return wpa_ctrl_command(ctrl, "MOBILE", 1, argc, argv);
 }
 
 static int hostapd_cli_cmd_set_neighbor(struct wpa_ctrl *ctrl, int argc,
