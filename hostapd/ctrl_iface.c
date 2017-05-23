777767e912a88f6a7e27e083e291bdcb7cf35349
jgarciam4678 <jesus.garciam@pucp.edu.pe>
2017-05-23 18:58:25 -0500

    Update ctrl_iface.c

--- hostapd/ctrl_iface.c
+++ hostapd/ctrl_iface.c
@@ -2605,8 +2605,8 @@ static int hostapd_ctrl_iface_receive_process(struct hostapd_data *hapd,
 	} else if (os_strncmp(buf, "PMKSA_ADD ", 10) == 0) {
 		if (hostapd_ap_add_pmksa(hapd, buf + 10)<0)
 			reply_len = -1;
-	} else if (os_strncmp(buf, "MOBILE ", 7) == 0) {
-		if (hostapd_ap_add_pmksa(hapd, buf + 7)<0)
+	} else if (os_strncmp(buf, "PMKSA_CREATE ", 13) == 0) {
+		if (hostapd_ctrl_iface_pmksa_create_entry(hapd, buf + 13)<0)
 			reply_len = -1;
 	} else if (os_strcmp(buf, "HELLOWORLD") == 0) { 
 		os_memcpy(reply, "Hell! O' world, why won't my code compile?\n\n", 46); 
